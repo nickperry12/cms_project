98295775e55dbcaaed720668ee47b00948ffc1aa
Nick Perry <nick.perry12@outlook.com>
2023-10-20 13:46:18 -0700

    updating and add project files

--- cms_proj.rb
+++ cms_proj.rb
@@ -4,23 +4,45 @@ require 'sinatra/content_for'
 require 'tilt/erubis'
 require 'redcarpet'
 
-abs_path = File.expand_path("..", __FILE__)
-
+# renders the content of a markdown file
 def render_markdown(text)
   markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
   markdown.render(text)
 end
 
+# decides how to load content based on the file type
 def load_content(file)
   content = File.read(file)
 
   case File.extname(file)
   when ".md"
-    render_markdown(content)
+    erb render_markdown(content)
   when ".txt"
     headers["Content-Type"] = "text/plain"
     content
   end
+end
+
+def data_path
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("./tests/project_files")
+  else
+    File.expand_path("./public/project_files")
+  end
+end
+
+def valid_file_name?(name)
+  if (name =~ /[^A-Za-z0-9\-_.]+/) == nil
+    session[:success] = "#{name} was created."
+    true
+  else
+    session[:error] = "A name is required (no special characters)."
+    false
+  end
+end
+
+def validate_credentials(username, password)
+  username == "admin" && password == "secret"
 end
 
 configure do
@@ -29,23 +51,113 @@ configure do
 end
 
 before do
-  @files = Dir.glob(abs_path + "/public/project_files/*").map do |file|
+  # finds all files in the provided directory and shortens the name to their
+  # base name
+  pattern = File.join(data_path, "*")
+  @files = Dir.glob(pattern).map do |file|
     File.basename(file)
   end
 end
 
+# renders the index page
 get "/" do
   erb :index, layout: :layout
 end
 
+# render page for create a new document
+get "/new" do
+  if session[:username] == "admin"
+    erb :new, layout: :layout
+  else
+    session[:error] = "You must be signed in to do that."
+    redirect "/"
+  end
+end
+
+# creates new document
+post "/new" do
+  file_name = params[:new_doc]
+  if valid_file_name?(file_name)
+    File.write(File.join(data_path, file_name), "")
+    redirect "/"
+  else
+    erb :new, layout: :layout
+  end
+end
+
+# renders sign in page
+get "/users/sign_in" do
+  erb :sign_in, layout: :layout
+end
+
+# signs the user in
+post "/users/sign_in" do
+  username = params[:username].to_s
+  password = params[:password].to_s
+
+  if validate_credentials(username, password)
+    session[:signed_in] = "Signed in as admin."
+    session[:username] = username
+    redirect "/"
+  else
+    session[:invalid_credentials] = "Invalid credentials."
+    erb :sign_in, layout: :layout
+  end
+end
+
+# signs the user out
+get "/users/sign_out" do
+  session[:sign_out] = "You have been signed out."
+  session[:signed_in] = nil
+  redirect "/"
+end
+
+# renders the file content of the chosen file
 get "/:file_name" do
   file_name = params[:file_name].to_s
-  file_path = "./public/project_files/#{file_name}"
+  file_path = File.join(data_path, file_name)
 
   if !@files.include?(file_name)
     session[:error] = "#{file_name} does not exist."
     redirect "/"
   else
     load_content(file_path)
+  end
+end
+
+# edit the file of choice
+get "/:file_name/edit" do
+  if session[:username]
+    @file_name = params[:file_name].to_s
+    file_path = File.join(data_path, @file_name)
+    @file_content = File.read(file_path)
+    erb :edit_file, layout: :layout
+  else
+    session[:error] = "You must be signed in to do that."
+    redirect "/"
+  end
+end
+
+# pushes the changes to the respective file
+post "/:file_name" do
+  file_name = params[:file_name].to_s
+  file_path = File.join(data_path, file_name)
+  content = params[:content]
+  File.write(file_path, content)
+  session[:success] = "#{params[:file_name]} has been updated."
+
+  redirect "/"
+end
+
+post "/:file_name/delete" do
+  if session[:username]
+    file_name = params[:file_name].to_s
+    file_path = File.join(data_path, file_name)
+    File.delete(file_path)
+    session[:success] = "#{file_name} was deleted."
+    redirect "/"
+  else
+    session[:error] = "You must be signed in to do that."
+    redirect "/"
   end
 end
--- tests/cms_test.rb
+++ tests/cms_test.rb
@@ -3,6 +3,7 @@ ENV["RACK_ENV"] = "test" # used by Sinatra and Rack to know if code is being tes
 require "minitest/autorun"
 require "minitest/reporters"
 require "rack/test"
+require "fileutils"
 Minitest::Reporters::use!
 
 require_relative "../cms_proj"
@@ -14,7 +15,29 @@ class CMSTest < Minitest::Test
     Sinatra::Application
   end
 
+  def setup
+    FileUtils.mkdir_p(data_path)
+  end
+
+  def teardown
+    FileUtils.rm_rf(data_path)
+  end
+
+  def create_document(name, content = "")
+    File.open(File.join(data_path, name), "w") do |file|
+      file.write(content)
+    end
+  end
+
+  def session
+    last_request.env["rack.session"]
+  end
+
   def test_index
+    create_document("changes.txt")
+    create_document("about.txt")
+    create_document("history.txt")
+
     get "/"
     assert_equal(200, last_response.status)
     assert_equal("text/html;charset=utf-8", last_response["Content-Type"])
@@ -24,9 +47,17 @@ class CMSTest < Minitest::Test
   end
 
   def test_file_content
-    history_content = File.read("./public/project_files/history.txt")
-    about_content = File.read("./public/project_files/about.txt")
-    changes_content = File.read("./public/project_files/changes.txt")
+    create_document("changes.txt")
+    create_document("about.txt")
+    create_document("history.txt")
+
+    changes = File.join(data_path, "changes.txt")
+    history = File.join(data_path, "history.txt")
+    about = File.join(data_path, "about.txt")
+    
+    history_content = File.read(history)
+    about_content = File.read(about)
+    changes_content = File.read(changes)
     
     get "/history.txt"
     assert_equal(200, last_response.status)
@@ -45,24 +76,93 @@ class CMSTest < Minitest::Test
   end
 
   def test_document_not_found
-    filename = "notafile.txt"
-    error_message = "#{filename} does not exist."
-    get "/#{filename}"
+    get "notafile.txt"
+
     assert_equal(302, last_response.status)
-
-    get last_response["Location"]
-    assert_equal(200, last_response.status)
-    assert_includes(last_response.body, error_message)
-
-    get "/"
-    assert_equal(200, last_response.status)
-    refute_includes(last_response.body, error_message)
+    assert_equal("notafile.txt does not exist.", session[:error])
   end
 
   def test_markdown_content
+    create_document("brady.md", "<h1>Tom Brady...</h1>")
+
     get "/brady.md"
+
     assert_equal(200, last_response.status)
     assert_equal("text/html;charset=utf-8", last_response["Content-Type"])
     assert_includes(last_response.body, "<h1>Tom Brady...</h1>")
+  end
+
+  def test_edit_document
+    post "/changes.txt", content: "new content"
+
+    assert_equal 302, last_response.status
+    assert_equal "changes.txt has been updated.", session[:success]
+
+    get "/changes.txt"
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "new content"
+  end
+
+  def test_create_document
+    get "/new"
+    assert_equal(200, last_response.status)
+    assert_includes(last_response.body, "<input")
+    assert_includes(last_response.body, %q(<button class="create" type="submit"))
+
+    post "/new", new_doc: "newfile.txt"
+
+    assert_equal(302, last_response.status)
+    assert_equal("newfile.txt was created.", session[:success])
+
+    get "/"
+
+    assert_equal(200, last_response.status)
+    assert_includes(last_response.body, "newfile.txt")
+  end
+
+  def test_delete_document
+    create_document("new_doc.txt")
+    file_name = "new_doc.txt"
+    file_path = File.join(data_path, file_name)
+
+    post "/#{file_name}/delete"
+    
+    assert_equal(302, last_response.status)
+    assert_equal("new_doc.txt was deleted.", session[:success])
+    get "/"
+
+    assert_equal(200, last_response.status)
+    assert_nil(session[:success])
+
+    get "/"
+
+    refute_includes(last_response.body, "new_doc.txt")
+  end
+
+  def test_signin_form
+    get "/users/sign_in"
+
+    assert_equal(200, last_response.status)
+    assert_includes(last_response.body, "<input")
+    assert_includes(last_response.body, %q(<button type="submit"))
+  end
+
+  def test_successful_sign_in
+    post "/users/sign_in", username: "admin", password: "secret"
+    
+    assert_equal(302, last_response.status)
+    assert_equal("Signed in as admin.", session[:signed_in])
+    assert_equal("admin", session[:username])
+
+    get last_response["Location"]
+
+    assert_includes(last_response.body, "<a href=\"/users/sign_out\">Sign Out</a>")
+  end
+
+  def test_unsuccessful_sign_in
+    post "/users/sign_in", username: "hocus", password: "pocus"
+    assert_equal(200, last_response.status)
+    assert_nil(session[:invalid_credentials])
+    assert_includes(last_response.body, "Invalid credentials.")
   end
 end
